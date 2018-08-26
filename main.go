1ad47dce040d0f61024b5736f529c32a56a41ee4
n-inja <mccreepertakumi@gmail.com>
2018-08-26 22:55:52 +0900

    end

--- main.go
+++ main.go
@@ -1,5 +1,106 @@
 package main
 
-func main () {
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
 
+	"os"
+
+	"io/ioutil"
+
+	"encoding/json"
+
+	"errors"
+	"strconv"
+
+	"./utils"
+)
+
+type Port struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
+
+func main() {
+	databaseAddress := ""
+	if os.Getenv("DATABASE_ADDRESS") != "" {
+		databaseAddress = os.Getenv("DATABASE_ADDRESS")
+	}
+
+	// connect database
+	err := utils.Open(os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), databaseAddress, os.Getenv("DATABASE_NAME"))
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+	defer utils.Close()
+
+	// load port data
+	bytes, err := ioutil.ReadFile("./conf/data.json")
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	// decode json file
+	var Ports []Port
+	if err := json.Unmarshal(bytes, &Ports); err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	if len(Ports) == 0 {
+		return
+	}
+
+	// create reverse proxy
+	errorChannel := make(chan error)
+	for _, port := range Ports {
+		go func() {
+			// check port
+			if num, err := strconv.Atoi(port.From); err != nil || num < 0 || num > 65535 {
+				errorChannel <- errors.New("json error")
+			}
+			if num, err := strconv.Atoi(port.To); err != nil || num < 0 || num > 65535 {
+				errorChannel <- errors.New("json error")
+			}
+
+			// request
+			director := func(request *http.Request) {
+				request.URL.Scheme = "http"
+				request.URL.Host = ":" + port.To
+				if request.Header.Get("transparent-proxy") == "true" {
+					errorChannel <- errors.New("loop detected")
+				}
+				request.Header.Set("transparent-proxy", "true")
+
+				// certification
+				id := ""
+				cookie, err := request.Cookie("session")
+				if err == nil && cookie.Value != "" {
+					id, err = utils.CheckSession(cookie.Value)
+					if err != nil {
+						id = ""
+					}
+				}
+
+				request.Header.Set("id", id)
+			}
+
+			// server
+			rp := &httputil.ReverseProxy{Director: director}
+			server := http.Server{Addr: ":" + port.From, Handler: rp}
+			errorChannel <- server.ListenAndServe()
+		}()
+	}
+
+	// catch goroutines error
+	for {
+		err = <-errorChannel
+		if err != nil {
+			log.Fatal(err.Error())
+			return
+		}
+	}
 }
---
+++ utils/db.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"database/sql"
+	"time"
+
+	"errors"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func Open(userName, password, address, databaseName string) error {
+	var err error
+	db, err = sql.Open("mysql", userName+":"+password+"@"+address+"/"+databaseName)
+	if err != nil {
+		return err
+	}
+	return initDB()
+}
+
+func Close() {
+	db.Close()
+}
+
+func initDB() error {
+	rows, err := db.Query("show tables like 'sessions'")
+	if err != nil {
+		return err
+	}
+	if !rows.Next() {
+		return errors.New("session table not found")
+	}
+	rows.Close()
+	return nil
+}
+
+func CheckSession(session string) (string, error) {
+	now := time.Now()
+	rows, err := db.Query("select id from sessions where session = ? and expiration_date > ?", session, now.Format("2016-01-02 15:04:05"))
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	if !rows.Next() {
+		return "", nil
+	}
+	var ID string
+	rows.Scan(&ID)
+	return ID, nil
+}
